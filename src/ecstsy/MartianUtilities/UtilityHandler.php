47e25cea4eacf8d27db5609bc83ded3d055356bb
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2025-01-02 17:40:44 -0800

    Add files via upload

---
+++ src/ecstsy/MartianUtilities/UtilityHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ecstsy\MartianUtilities;
+
+use InvalidArgumentException;
+use pocketmine\plugin\Plugin;
+
+final class UtilityHandler {
+
+    private static ?Plugin $plugin = null;
+
+    public static function register(Plugin $plugin): void {
+        if (self::isRegistered()) {
+            throw new InvalidArgumentException("{$plugin->getName()} attempted to registe " . self::class . " twice.");
+        }
+
+        self::$plugin = $plugin;
+    }
+
+    public static function isRegistered(): bool {
+        return self::$plugin instanceof Plugin;
+    }
+}
---
+++ src/ecstsy/MartianUtilities/utils/InventoryUtils.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ecstsy\MartianUtilities\utils;
+
+use pocketmine\inventory\Inventory;
+use pocketmine\item\Item;
+
+class InventoryUtils {
+
+    /**
+     * Fill the borders of the inventory with gray glass.
+     *
+     * @param Inventory $inventory
+     */
+    public static function fillBorders(Inventory $inventory, Item $glassType, array $excludedSlots = []): void
+    {
+        $size = $inventory->getSize();
+        $rows = intdiv($size, 9); 
+    
+        for ($row = 0; $row < $rows; $row++) {
+            for ($col = 0; $col < 9; $col++) {
+                $slot = $row * 9 + $col;
+    
+                if (!in_array($slot, $excludedSlots) && ($col === 0 || $col === 8 || $row === 0 || $row === $rows - 1)) {
+                    $item = clone $glassType;
+                    $item->setCustomName(" ");
+                    $inventory->setItem($slot, $item);
+                }
+            }
+        }
+    }
+}
